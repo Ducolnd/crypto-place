3ea06556bd0bfaad36b14b1c30747ea1b3fa8a0c
Duco Lindhout <duco.lindhout@gmail.com>
2021-12-20 16:58:19 +0100

    Allow for react code

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,28 @@ const path = require('path');
 module.exports = {
     entry: {
         canvas: "./static/js/sidebar.js",
+        react: "./static/js/react.js",
     },
     output: {
         path: path.resolve(__dirname, "static", 'dist'),
     },
     mode: "development",
     devtool: "source-map",
+
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
 
     experiments: {
         asyncWebAssembly: true,
